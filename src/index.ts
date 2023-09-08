6a74f49a5444ae5642e3d1112ba6815aef81dbbb
anon <anon@anoncom.net>
2023-09-09 03:00:28 +0900

    Update cache images by R2

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,112 @@
-import { Hono, Context } from "hono";
-import { AtpAgent } from "@atproto/api";
+import { Hono } from 'hono/quick'
+import { Context } from 'hono'
+import { AtpAgent } from '@atproto/api'
 
-let atp: AtpAgent | null = null;
+type Bindings = {
+  BUCKET: R2Bucket
+}
 
-const app = new Hono();
 
-const proxy = new Hono();
+const app = new Hono<{Bindings: Bindings}>()
 
-app.get("/ping", (c: Context) => c.text("pong"));
+const proxy = new Hono()
 
-proxy.get("/:service/image", (c: Context) => c.text("No image"));
-proxy.get("/:service/image/:did/:cid", async (c: Context) => {
-  const { service, did, cid } = c.req.param();
+
+
+app.get('/ping', (c: Context) => c.text('pong'))
+
+proxy.get('/:service/image', (c: Context) => c.text('No image'))
+
+
+proxy.get('/:service/image/:did/:cid', async (c: Context) => {
+  const { service, did, cid } = c.req.param()
 
   console.log(`target Service = ${service}`)
-  console.log(`did = ${did}`, `cid = ${cid}`);
+  console.log(`did = ${did}`, `cid = ${cid}`)
 
-  const cache = caches.default;
-
-  const cacheKey = `${c.req.url}.blob`;
-  console.log("cacheKey = ", cacheKey);
+  const cache = caches.default
+  const cacheAge = 60 * 60 * 24 * 3 // 3 days
+  const cacheKey = `${service}/${did.replaceAll(':','/')}/${cid}`
+  console.log(`cacheKey = ${cacheKey}`)
 
   try {
-    const getAgent = () => {
-      atp = new AtpAgent({ service: `https://${service}` });
-      return atp;
-    };
-    
-    let response = await cache.match(cacheKey);
+    // Cache hit
+    //let response = await cache.match(c.req.url)
+    let response = null
 
     if (!response) {
-      const blobResp = await getAgent().api.com.atproto.sync.getBlob({
-        did: did,
-        cid: cid,
-      });
-      if (!blobResp.success) throw new Error("Not found");
+      try {
+        // Get from Cloudflare R2
+        console.log('Get from R2')
+        // let object = await c.env.BUCKET.head(cacheKey)
+        let object = await c.env.BUCKET.get(cacheKey)
+        console.debug(object)
+        // object = null
+        if (object && object.size > 0) {
+          console.info('R2: Object found!')
+          const data = await object.arrayBuffer()
+          response = new Response(data, {
+            status: 200,
+            headers: {
+              'Content-Type': object.httpMetadata?.contentType,
+              'Content-Length': object.httpMetadata?.contentLength,
+              'Cache-Control': `public, max-age=${cacheAge}, immutable`,
+            },
+          })
+        } else {
+          
+          // Fetch from PDS repo
 
-      const blobObject = new Blob([blobResp.data], {
-        type: blobResp.headers["Content-Type"] as string,
-      });
+          const getAgent = () => {
+            return new AtpAgent({ service: `https://${service}` })
+          }
 
-      //URL.createObjectURL(blobObject)
-      const cacheAge = 60 * 60 * 24 * 7 // 1 week
-      response = new Response(blobObject, blobResp.headers);
-      response.headers.append("Cache-Control", `s-maxage=${cacheAge}`);
+          const blobResp = await getAgent().api.com.atproto.sync.getBlob({
+            did: did,
+            cid: cid,
+          })
+          if (!blobResp.success) throw new Error('Not found')
 
-      c.executionCtx.waitUntil(cache.put(cacheKey, response.clone()));
+          const contentType = blobResp.headers['content-type']
+
+          // console.debug(blobResp.headers)
+          console.debug('start put to R2')
+          const putResult = await c.env.BUCKET.put(cacheKey, blobResp.data, {
+            httpMetadata: {
+              contentType: contentType,
+              contentLength: blobResp.headers['content-length'],
+            }
+          })
+          console.log('R2: put result = ', putResult)
+          if (putResult == null) throw new Error('Failed to put objects to R2')
+          console.debug('finish put to R2')
+
+          const blob = new Blob([blobResp.data], {
+            type: contentType,
+          })
+          // Create response
+          response = new Response(blob, blobResp.headers)
+          response.headers.set('Cache-Control', `public, max-age=${cacheAge}, immutable`)
+          c.executionCtx.waitUntil(cache.put(c.req.url, response.clone()))
+        }
+      } catch (error) {
+        console.error(error)
+        throw error
+      }
+
     }
 
-    return response;
+    return response
+
+
   } catch (error) {
-    console.error(error);
-    c.status(404);
-    c.text("Not found");
-    return c.res;
+    console.error(error)
+    c.status(404)
+    c.text('Not found')
+    return c.res
   }
 });
 
-app.route("/proxy", proxy);
+app.route('/proxy', proxy)
 
-export default app;
+export default app
